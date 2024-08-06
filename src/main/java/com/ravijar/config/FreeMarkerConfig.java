cc1e1c7c86750f216ae4d28bde83654d4c95396f
ravijar <ravijarandinu@gmail.com>
2024-08-06 10:15:23 +0530

    fixed issue with loading resources in jar file

--- src/main/java/com/ravijar/config/FreeMarkerConfig.java
+++ src/main/java/com/ravijar/config/FreeMarkerConfig.java
@@ -3,14 +3,13 @@ package com.ravijar.config;
 import freemarker.template.Configuration;
 import freemarker.template.TemplateExceptionHandler;
 
-import java.io.File;
 import java.io.IOException;
 
 public class FreeMarkerConfig {
 
     public Configuration getConfiguration() throws IOException {
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_33);
-        cfg.setDirectoryForTemplateLoading(new File("src/main/resources/templates/freemarker"));
+        cfg.setClassForTemplateLoading(FreeMarkerConfig.class, "/templates/freemarker");
         cfg.setDefaultEncoding("UTF-8");
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
         cfg.setLogTemplateExceptions(false);

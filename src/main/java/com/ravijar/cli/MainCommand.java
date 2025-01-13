6e3a5812e6d2f96abe77e76dd44faf9a5e2b218e
IP Jayawickrama <70957000+ipjayawick@users.noreply.github.com>
2025-01-13 13:53:14 +0530

    Update version number

--- src/main/java/com/ravijar/cli/MainCommand.java
+++ src/main/java/com/ravijar/cli/MainCommand.java
@@ -5,13 +5,13 @@ import picocli.CommandLine.Command;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Command(name = "fegen", version = "FeGenerator 1.0", mixinStandardHelpOptions = true)
+@Command(name = "fegen", version = "FeGenerator 0.2.0", mixinStandardHelpOptions = true)
 public class MainCommand implements Runnable {
     private static final Logger logger = LogManager.getLogger("entryLogger");
 
     @Override
     public void run() {
-        logger.info(Ansi.ansi().bold().fgBright(Ansi.Color.WHITE).a("Welcome to FeGenerator 1.0!").reset());
+        logger.info(Ansi.ansi().bold().fgBright(Ansi.Color.WHITE).a("Welcome to FeGenerator 0.2.0!").reset());
         logger.info(Ansi.ansi().fgCyan().a("Frontend Generator CLI Tool").reset());
         logger.info("For help and available commands, use: {}",
                 Ansi.ansi().bold().a("java -jar fegen.jar --help").reset());

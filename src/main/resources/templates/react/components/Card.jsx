e33fd11889a09bdfb2fb77eab6d28a27fb02dc10
ravijar <ravijarandinu@gmail.com>
2025-02-02 16:07:11 +0530

    updated Card styles

--- src/main/resources/templates/react/components/Card.jsx
+++ src/main/resources/templates/react/components/Card.jsx
@@ -5,19 +5,30 @@ import {Children, cloneElement, isValidElement} from "react";
 const Card = ({ item, displayNames, styles = {}, children }) => {
     return (
         <div className="detailed-card" style={styles.detailedCard}>
-            <RecursiveKeyValuePair data={item} displayNames={displayNames}/>
-            <div className="children-container" style={styles.childrenContainer}>
-                {Children.map(children, (child) =>
-                    isValidElement(child)
-                        ? cloneElement(child, {
-                            children: Children.map(child.props.children, (nestedChild) =>
-                                isValidElement(nestedChild)
-                                    ? cloneElement(nestedChild, { id: item.id })
-                                    : nestedChild
-                            ),
-                        })
-                        : child
-                )}
+            <div className="detailed-card-img" style={styles.cardImg}>
+                <img src={item.image} alt={item.title}></img>
+            </div>
+            <div className="content-container" style={styles.contentContainer}>
+                <div className="detailed-card-info" style={styles.detailedCardInfo}>
+                    <p className="detailed-card-title" style={styles.detailedCardTitle}>{item.title}</p>
+                    <p className="detailed-card-body" style={styles.detailedCardBody}>{item.description}</p>
+                </div>
+                <div className="key-value-pairs-container" style={styles.keyValuePairsContainer}>
+                    <RecursiveKeyValuePair data={item.data} displayNames={displayNames}/>
+                </div>
+                <div className="children-container" style={styles.childrenContainer}>
+                    {Children.map(children, (child) =>
+                        isValidElement(child)
+                            ? cloneElement(child, {
+                                children: Children.map(child.props.children, (nestedChild) =>
+                                    isValidElement(nestedChild)
+                                        ? cloneElement(nestedChild, {id: item.id})
+                                        : nestedChild
+                                ),
+                            })
+                            : child
+                    )}
+                </div>
             </div>
         </div>
     );
